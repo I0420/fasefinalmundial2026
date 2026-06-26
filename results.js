34a950761342121d150c83058ed413e280d490e1
IO0420 <natilo.1994@gmail.com>
2026-06-26 05:28:36 -0500

    Add files via upload

---
+++ results.js
@@ -0,0 +1,47 @@
+const RESULTS = {
+  "knockout": {
+    "matches": {
+      "round32": [
+        { "match": 1, "team1": "Ecuador", "team2": "Argentina" },
+        { "match": 2, "team1": "Spain", "team2": "Colombia" },
+        { "match": 3, "team1": "Germany", "team2": "France" },
+        { "match": 4, "team1": "Brazil", "team2": "Japan" },
+        { "match": 5, "team1": "Mexico", "team2": "USA" },
+        { "match": 6, "team1": "England", "team2": "Senegal" },
+        { "match": 7, "team1": "Netherlands", "team2": "Morocco" },
+        { "match": 8, "team1": "Portugal", "team2": "Switzerland" },
+        { "match": 9, "team1": "Belgium", "team2": "Canada" },
+        { "match": 10, "team1": "Croatia", "team2": "Italy" },
+        { "match": 11, "team1": "Denmark", "team2": "Czech Republic" },
+        { "match": 12, "team1": "South Korea", "team2": "Uruguay" },
+        { "match": 13, "team1": "Poland", "team2": "Austria" },
+        { "match": 14, "team1": "Greece", "team2": "Turkey" },
+        { "match": 15, "team1": "Sweden", "team2": "Norway" },
+        { "match": 16, "team1": "Ukraine", "team2": "Wales" }
+      ],
+      "round16": [
+        { "match": 17, "team1": "Ganador 1", "team2": "Ganador 2" },
+        { "match": 18, "team1": "Ganador 3", "team2": "Ganador 4" },
+        { "match": 19, "team1": "Ganador 5", "team2": "Ganador 6" },
+        { "match": 20, "team1": "Ganador 7", "team2": "Ganador 8" },
+        { "match": 21, "team1": "Ganador 9", "team2": "Ganador 10" },
+        { "match": 22, "team1": "Ganador 11", "team2": "Ganador 12" },
+        { "match": 23, "team1": "Ganador 13", "team2": "Ganador 14" },
+        { "match": 24, "team1": "Ganador 15", "team2": "Ganador 16" }
+      ],
+      "quarterfinals": [
+        { "match": 25, "team1": "Ganador 17", "team2": "Ganador 18" },
+        { "match": 26, "team1": "Ganador 19", "team2": "Ganador 20" },
+        { "match": 27, "team1": "Ganador 21", "team2": "Ganador 22" },
+        { "match": 28, "team1": "Ganador 23", "team2": "Ganador 24" }
+      ],
+      "semifinals": [
+        { "match": 29, "team1": "Ganador 25", "team2": "Ganador 26" },
+        { "match": 30, "team1": "Ganador 27", "team2": "Ganador 28" }
+      ],
+      "final": [
+        { "match": 31, "team1": "Ganador 29", "team2": "Ganador 30" }
+      ]
+    }
+  }
+};
